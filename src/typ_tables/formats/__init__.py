45a472c07f2994f779d3bd675fdee9e575a56a5c
Ben Gale <bengale2007@googlemail.com>
2026-03-29 13:53:56 +0100

    Refactored numeric formatting logic

---
+++ src/typ_tables/formats/__init__.py
@@ -0,0 +1,149 @@
+"""Module containing cell formatting.
+
+Numeric formatting code is taken from Great-Tables and adapted slightly.
+"""
+
+import typing as t
+from dataclasses import dataclass
+
+import narwhals as nw
+
+from typ_tables import location, ttypes
+from typ_tables.constants import ROW_INDEX
+from typ_tables.formats import _numeric
+
+
+class FormatFn(t.Protocol):
+    """Protocol defining a formatting function."""
+
+    def fmt(self, data: ttypes.Data, cols: list[str], rows: list[int]) -> ttypes.Data:
+        """Formats the data in the given columns and rows."""
+
+
+@dataclass
+class Formatter:
+    """A Formatter container."""
+
+    func: FormatFn
+    cols: list[str]
+    rows: list[int]
+
+    def fmt(self, data: ttypes.Data) -> ttypes.Data:
+        """Formats the given data."""
+        return self.func.fmt(data, self.cols, self.rows)
+
+
+def fmt(
+    data: ttypes.Data,
+    func: FormatFn,
+    columns: location.ColumnSelector | None = None,
+    rows: location.RowSelector | None = None,
+) -> Formatter:
+    """Create a formatter container."""
+    cols = location.resolve_columns(data, columns)
+    rows = location.resolve_rows(data, rows)
+
+    return Formatter(func, cols, rows)
+
+
+def _create_when(col: str, rows: list[int], then: nw.Expr, when: nw.Expr | None = None) -> nw.Expr:
+    whens = [nw.col(ROW_INDEX).is_in(rows)]
+    if when is not None:
+        whens.append(when)
+    return nw.when(*whens).then(then).otherwise(nw.col(col).cast(nw.String)).alias(col)
+
+
+@dataclass
+class SubMissing:
+    """Substitutes missing values in a table."""
+
+    missing_text: str = ""
+
+    def fmt(self, data: ttypes.Data, cols: list[str], rows: list[int]) -> ttypes.Data:
+        """Formatting missing values in the given columns and rows."""
+        whens = []
+        for col in cols:
+            if data.select(nw.col(col)).schema[col].is_numeric():
+                cond = nw.col(col).is_null() | nw.col(col).is_nan()
+            else:
+                cond = nw.col(col).is_null() | (nw.col(col) == nw.lit("nan"))
+
+            when = _create_when(col, rows, nw.lit(self.missing_text), cond)
+            whens.append(when)
+        return data.with_columns(whens)
+
+
+@dataclass
+class FString:
+    """Formats columns using the Polars fstring format."""
+
+    f_string: str
+
+    def fmt(self, data: ttypes.Data, cols: list[str], rows: list[int]) -> ttypes.Data:
+        """Formats values in the given columns and rows."""
+        whens = [_create_when(col, rows, nw.format(self.f_string, nw.col(col))) for col in cols]
+        return data.with_columns(whens)
+
+
+@dataclass
+class Numeric:
+    """Format numeric columns."""
+
+    decimals: int
+    n_sigfig: int | None
+    drop_trailing_zeros: bool
+    drop_trailing_dec_mark: bool
+    use_seps: bool
+    accounting: bool
+    scale_by: float
+    compact: bool
+    sep_mark: str
+    dec_mark: str
+    force_sign: bool
+    pattern: str
+
+    def fmt(self, data: ttypes.Data, cols: list[str], rows: list[int]) -> ttypes.Data:
+        """Formatting numeric values in the given columns and rows."""
+        unique_rows = set(rows)
+        new_cols = []
+        for col in cols:
+            new_col = nw.new_series(
+                name=col,
+                values=[self.fmt_value(value) if i in unique_rows else str(value) for i, value in enumerate(data[col])],
+                backend=data.implementation,
+            )
+            new_cols.append(new_col)
+        return data.with_columns(*new_cols)
+
+    def fmt_value(self, value: object) -> str | None:
+        """Formats an individual value."""
+        if isinstance(value, str):
+            try:
+                value = float(value)
+            except ValueError:
+                return value
+        elif value is None:
+            return None
+        elif not isinstance(value, float):
+            return str(value)
+
+        nan_or_inf = _numeric.is_nan_or_inf(value)
+
+        value = value * self.scale_by
+
+        is_negative = value < 0
+
+        if nan_or_inf:
+            x_formatted = str(value)
+        elif self.compact:
+            x_formatted = _numeric.format_number_compactly(value=value, config=self)
+        else:
+            x_formatted = _numeric.value_to_decimal_notation(value=value, config=self)
+
+        if is_negative and self.accounting:
+            x_formatted = f"({_numeric.remove_minus(x_formatted)})"
+
+        if self.pattern != "{x}":
+            x_formatted = self.pattern.replace("{x}", x_formatted)
+
+        return x_formatted
--- src/typ_tables/formats/_numeric.py
+++ src/typ_tables/formats/_numeric.py
@@ -1,175 +1,22 @@
-"""Module containing cell formatting.
-
-Numeric formatting code is taken from Great-Tables and adapted slightly.
-"""
+"""Numeric formatting helper functions."""
 
 import math
 import typing as t
-from dataclasses import dataclass
 
-import narwhals as nw
-
-from typ_tables import location, ttypes
-from typ_tables.constants import ROW_INDEX
+if t.TYPE_CHECKING:
+    from typ_tables.formats import Numeric
 
 
-class FormatFn(t.Protocol):
-    """Protocol defining a formatting function."""
-
-    def fmt(self, data: ttypes.Data, cols: list[str], rows: list[int]) -> ttypes.Data:
-        """Formats the data in the given columns and rows."""
-
-
-@dataclass
-class Formatter:
-    """A Formatter container."""
-
-    func: FormatFn
-    cols: list[str]
-    rows: list[int]
-
-    def fmt(self, data: ttypes.Data) -> ttypes.Data:
-        """Formats the given data."""
-        return self.func.fmt(data, self.cols, self.rows)
-
-
-def fmt(
-    data: ttypes.Data,
-    func: FormatFn,
-    columns: location.ColumnSelector | None = None,
-    rows: location.RowSelector | None = None,
-) -> Formatter:
-    """Create a formatter container."""
-    cols = location.resolve_columns(data, columns)
-    rows = location.resolve_rows(data, rows)
-
-    return Formatter(func, cols, rows)
-
-
-def _create_when(col: str, rows: list[int], then: nw.Expr, when: nw.Expr | None = None) -> nw.Expr:
-    whens = [nw.col(ROW_INDEX).is_in(rows)]
-    if when is not None:
-        whens.append(when)
-    return nw.when(*whens).then(then).otherwise(nw.col(col).cast(nw.String)).alias(col)
-
-
-@dataclass
-class SubMissing:
-    """Substitutes missing values in a table."""
-
-    missing_text: str = ""
-
-    def fmt(self, data: ttypes.Data, cols: list[str], rows: list[int]) -> ttypes.Data:
-        """Formatting missing values in the given columns and rows."""
-        whens = []
-        for col in cols:
-            if data.select(nw.col(col)).schema[col].is_numeric():
-                cond = nw.col(col).is_null() | nw.col(col).is_nan()
-            else:
-                cond = nw.col(col).is_null() | (nw.col(col) == nw.lit("nan"))
-
-            when = _create_when(col, rows, nw.lit(self.missing_text), cond)
-            whens.append(when)
-        return data.with_columns(whens)
-
-
-@dataclass
-class FString:
-    """Formats columns using the Polars fstring format."""
-
-    f_string: str
-
-    def fmt(self, data: ttypes.Data, cols: list[str], rows: list[int]) -> ttypes.Data:
-        """Formatting missing values in the given columns and rows."""
-        whens = [_create_when(col, rows, nw.format(self.f_string, nw.col(col))) for col in cols]
-        return data.with_columns(whens)
-
-
-@dataclass
-class Numeric:
-    """Format numeric columns."""
-
-    decimals: int
-    n_sigfig: int | None
-    drop_trailing_zeros: bool
-    drop_trailing_dec_mark: bool
-    use_seps: bool
-    accounting: bool
-    scale_by: float
-    compact: bool
-    sep_mark: str
-    dec_mark: str
-    force_sign: bool
-    pattern: str
-
-    def fmt(self, data: ttypes.Data, cols: list[str], rows: list[int]) -> ttypes.Data:
-        """Formatting missing values in the given columns and rows."""
-        unique_rows = set(rows)
-        new_cols = []
-        for col in cols:
-            new_col = nw.new_series(
-                name=col,
-                values=[self.fmt_value(value) if i in unique_rows else str(value) for i, value in enumerate(data[col])],
-                backend=data.implementation,
-            )
-            new_cols.append(new_col)
-        return data.with_columns(*new_cols)
-
-    def fmt_value(self, value: object) -> object:
-        """Formats an individual value."""
-        if isinstance(value, str):
-            try:
-                value = float(value)
-            except ValueError:
-                return value
-        elif not isinstance(value, float):
-            return value
-
-        return fmt_number(value, config=self)
-
-
-def fmt_number(value: float, config: Numeric) -> str:
-    """Formats the given number using the config."""
-    nan = False
-    inf = False
-    try:
-        # NaN and Inf fails here.
-        int(value)
-    except ValueError:
-        nan = True
-    except OverflowError:
-        inf = True
-
-    value = value * config.scale_by
-
-    is_negative = value < 0
-
-    if nan or inf:
-        x_formatted = str(value)
-    elif config.compact:
-        x_formatted = _format_number_compactly(value=value, config=config)
-    else:
-        x_formatted = _value_to_decimal_notation(value=value, config=config)
-
-    if is_negative and config.accounting:
-        x_formatted = f"({_remove_minus(x_formatted)})"
-
-    if config.pattern != "{x}":
-        x_formatted = config.pattern.replace("{x}", x_formatted)
-
-    return x_formatted
-
-
-def _remove_minus(string: str) -> str:
+def remove_minus(string: str) -> str:
     return string.replace("-", "")
 
 
-def _format_number_compactly(value: float, config: Numeric) -> str:
+def format_number_compactly(value: float, config: "Numeric") -> str:
     if value == 0:
         return "0"
 
     if config.n_sigfig is not None:
-        _validate_n_sigfig(n_sigfig=config.n_sigfig)
+        validate_n_sigfig(n_sigfig=config.n_sigfig)
 
     # Determine the power index for the value and put it in the range of 0 to 5 which
     # corresponds to the list of suffixes `["", "K", "M", "B", "T", "Q"]`
@@ -185,7 +32,7 @@ def _format_number_compactly(value: float, config: Numeric) -> str:
 
     # Format the value to decimal notation; this is done before the `byte_units` text
     # is affixed to the value
-    x_formatted = _value_to_decimal_notation(value=value, config=config)
+    x_formatted = value_to_decimal_notation(value=value, config=config)
 
     # Create a `suffix_pattern` object for affixing the `units_str`, which is the
     # string that represents the 'K', 'M', 'B', 'T', or 'Q' suffix
@@ -194,7 +41,7 @@ def _format_number_compactly(value: float, config: Numeric) -> str:
     return suffix_pattern.replace("{x}", x_formatted)
 
 
-def _value_to_decimal_notation(value: float, config: Numeric) -> str:
+def value_to_decimal_notation(value: float, config: "Numeric") -> str:
     """Decimal notation.
 
     Returns a string value with the correct precision or fixed number of decimal places (with
@@ -206,7 +53,7 @@ def _value_to_decimal_notation(value: float, config: Numeric) -> str:
         # If there is a value provided to `n_sigfig` then number formatting proceeds through the
         # significant digits pathway, which ignores `decimals` and any removal of trailing zero values
         # in the decimal portion of the value
-        result = _format_number_n_sigfig(
+        result = format_number_n_sigfig(
             value=value,
             n_sigfig=config.n_sigfig,
             use_seps=config.use_seps,
@@ -218,7 +65,7 @@ def _value_to_decimal_notation(value: float, config: Numeric) -> str:
         # If there is nothing provided to `n_sigfig` then the conventional decimal number formatting
         # pathway is taken; this formats to a specific number of decimal places and removal of
         # trailing zero values can be undertaken
-        result = _format_number_fixed_decimals(
+        result = format_number_fixed_decimals(
             value=value,
             decimals=config.decimals,
             drop_trailing_zeros=config.drop_trailing_zeros,
@@ -239,7 +86,7 @@ def _value_to_decimal_notation(value: float, config: Numeric) -> str:
     return result
 
 
-def _format_number_n_sigfig(
+def format_number_n_sigfig(
     value: float,
     *,
     n_sigfig: int,
@@ -247,9 +94,9 @@ def _format_number_n_sigfig(
     sep_mark: str = ",",
     dec_mark: str = ".",
 ) -> str:
-    sig_digits, power, is_negative = _get_number_profile(value, n_sigfig)
+    sig_digits, power, is_negative = get_number_profile(value, n_sigfig)
 
-    formatted_value = ("-" if is_negative else "") + _insert_decimal_mark(digits=sig_digits, power=power, dec_mark=".")
+    formatted_value = ("-" if is_negative else "") + insert_decimal_mark(digits=sig_digits, power=power, dec_mark=".")
 
     # Get integer and decimal parts
     # Split number at `.` and obtain the integer and decimal parts
@@ -278,7 +125,7 @@ def _format_number_n_sigfig(
     return formatted_integer + formatted_decimal
 
 
-def _format_number_fixed_decimals(  # noqa: PLR0913
+def format_number_fixed_decimals(  # noqa: PLR0913
     value: float,
     *,
     decimals: int,
@@ -327,7 +174,7 @@ def _format_number_fixed_decimals(  # noqa: PLR0913
     return result
 
 
-def _get_number_profile(value: float, n_sigfig: int) -> tuple[str, int, bool]:
+def get_number_profile(value: float, n_sigfig: int) -> tuple[str, int, bool]:
     """Get key components of a number for decimal number formatting.
 
     Returns a tuple containing: (1) a string value of significant digits, (2) an
@@ -355,22 +202,22 @@ def _get_number_profile(value: float, n_sigfig: int) -> tuple[str, int, bool]:
     return sig_digits, -power, is_negative
 
 
-def _insert_decimal_mark(digits: str, power: int, dec_mark: str) -> str:
+def insert_decimal_mark(digits: str, power: int, dec_mark: str) -> str:
     """Places the decimal mark in the correct location within the digits.
 
     Should the decimal mark be outside the numeric range, zeros will be added.
     If `drop_trailing_zeros` is True, trailing decimal zeros will be removed.
 
     Examples:
-      _insert_decimal_mark("123",   2, False) => "12300"
-      _insert_decimal_mark("123",  -2, False) => "1.23"
-      _insert_decimal_mark("123",   3, False) => "0.123"
-      _insert_decimal_mark("123",   5, False) => "0.00123"
-      _insert_decimal_mark("120",   0, False) => "120."
-      _insert_decimal_mark("1200", -2, False) => "12.00"
-      _insert_decimal_mark("1200", -2, True ) => "12"
-      _insert_decimal_mark("1200", -1, False) => "120.0"
-      _insert_decimal_mark("1200", -1, True ) => "120"
+      insert_decimal_mark("123",   2, False) => "12300"
+      insert_decimal_mark("123",  -2, False) => "1.23"
+      insert_decimal_mark("123",   3, False) => "0.123"
+      insert_decimal_mark("123",   5, False) => "0.00123"
+      insert_decimal_mark("120",   0, False) => "120."
+      insert_decimal_mark("1200", -2, False) => "12.00"
+      insert_decimal_mark("1200", -2, True ) => "12"
+      insert_decimal_mark("1200", -1, False) => "120.0"
+      insert_decimal_mark("1200", -1, True ) => "120"
     """
     if power > 0:
         out = digits + "0" * power
@@ -391,7 +238,7 @@ def _insert_decimal_mark(digits: str, power: int, dec_mark: str) -> str:
     return out
 
 
-def _validate_n_sigfig(n_sigfig: int) -> None:  # pragma: no cover
+def validate_n_sigfig(n_sigfig: int) -> None:  # pragma: no cover
     """Validates the input for the number of significant figures.
 
     Args:
@@ -413,3 +260,16 @@ def _validate_n_sigfig(n_sigfig: int) -> None:  # pragma: no cover
     if n_sigfig < 1:
         msg = "The value for `n_sigfig` must be greater than or equal to `1`."
         raise ValueError(msg)
+
+
+NaNorInf = t.Literal["nan", "inf"] | None
+
+
+def is_nan_or_inf(value: float) -> NaNorInf:
+    try:
+        # NaN and Inf fails here.
+        int(value)
+    except ValueError:
+        return "nan"
+    except OverflowError:
+        return "inf"
